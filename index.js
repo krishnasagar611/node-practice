ea2cd343da11c9af9fe7ab8a4b2fa3a9d547cfb6
krishnasagar580@gmail.com <101728627+krishnasagar611@users.noreply.github.com>
2025-01-16 16:15:46 +0530

    INITIALISE PROJ

---
+++ index.js
@@ -0,0 +1,22 @@
+let express = require('express')
+let app = express();
+// console.log(app,"app")
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send({ status: 200, massage: "Hii this is home page api" })
+})
+
+app.post("/login", (req, res) => {
+    // console.log(req.body)
+
+    res.send({ status: 200, userName: "sagar", pasword: "krishna", data: (req.body), querydata: req.query }) 
+})
+
+//dynamic params 
+
+app.get("/me/:id",(req,res)=>{
+    res.send({status:200,msg:"me api"})
+})
+
+app.listen(`8000`)
