a3c0c7bee754b199d11671e8387c40d7b84567e5
krishnasagar580@gmail.com <101728627+krishnasagar611@users.noreply.github.com>
2025-01-21 12:27:57 +0530

    as

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-let express = require('express')
-let app = express();
-// console.log(app,"app")
-app.use(express.json())
+// let express = require('express')
+// let app = express();
+// // console.log(app,"app")
+// app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send({ status: 200, massage: "Hii this is home page api" })
-})
+// app.get("/", (req, res) => {
+//     res.send({ status: 200, massage: "Hii this is home page api" })
+// })
 
-app.post("/login", (req, res) => {
-    console.log(req.query)
+// app.post("/login", (req, res) => {
+//     console.log(req.query)
 
-    res.send({ status: 200, userName: "sagar", pasword: "krishna", data: (req.body), querydata: req.query }) 
-})
+//     res.send({ status: 200, userName: "sagar", pasword: "krishna", data: (req.body), querydata: req.query }) 
+// })
 
-//dynamic params 
+// //dynamic params 
 
-app.get("/me/:id",(req,res)=>{
-    res.send({status:200,msg:"me api"})
-})
+// app.get("/me/:id",(req,res)=>{
+//     res.send({status:200,msg:"me api"})
+// })
 
 
-app.listen(`8000`)
+
+
+// app.listen(`8000`)
+
+//closure 
+
+// function outerFunction() {
+//   let a = "sagar"
+//   function innerFuntion(){
+//     console.log("inner:--",a)
+//   }
+//   innerFuntion()
+// }
+// outerFunction()
+
+
+const outerFunction = () => {
+    let a = "sagar"
+    const innerFuntion = () => {
+        console.log(a)
+    }
+    return innerFuntion();
+    
+}
+outerFunction();
+
+
+
+
+
+
+
