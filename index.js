dcc6ea1b5c8b6ec9a7bfa60d4481fade9eb5dac7
krishnasagar580@gmail.com <101728627+krishnasagar611@users.noreply.github.com>
2025-01-23 16:10:48 +0530

    add

--- index.js
+++ index.js
@@ -36,15 +36,78 @@
 // outerFunction()
 
 
-const outerFunction = () => {
-    let a = "sagar"
-    const innerFuntion = () => {
-        console.log(a)
-    }
-    return innerFuntion();
-    
+//  const  outerFunction  = () => {
+//     let a = "sagar"
+//     const innerFuntion = () => {
+//         console.log(a)
+//     }
+//     return innerFuntion();
+
+// }
+// outerFunction();
+
+// async function foo() {
+//     await console.log("asasa");
+//   }
+//   foo()
+
+//   function foo() {
+//     return Promise.resolve(1).then(() => undefined);
+//   }
+
+// let text =10
+// function name1(text) {
+//     console.log(text)
+// }
+// name1(text)
+
+
+let obj = {
+  name:"sagar",
+  age:10
 }
-outerFunction();
+
+let PrintFullData = function(state,city){
+    // console.log("My name is ",this.name +" and my age is "+ this.age + " from " + state , city )
+}
+PrintFullData.call(obj)
+
+
+let obj2 = {
+    name:"krishna",
+    age:24
+  }
+//call
+PrintFullData.call(obj2,"mumbai","bandra")
+
+// apply
+
+PrintFullData.apply(obj2,["odisha","bhadrak"])
+
+// bind
+
+let BindFn = PrintFullData.bind(obj2,"mumbai","bandra")
+// console.log(BindFn)
+BindFn()
+
+let arr = [1,3,4454,64,33]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
